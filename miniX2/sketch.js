1b8a218816900033ac8d4d018d47ca626f8c405d
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2022-02-22 20:56:37 +0000

    color theory hard

--- miniX2/sketch.js
+++ miniX2/sketch.js
@@ -37,7 +37,7 @@ function setup() {
   angleMode(DEGREES);
 
 
-  expressionSlider = createSlider(5, 355, 177.5);
+  expressionSlider = createSlider(0, 355, 177.5);
   expressionSlider.position(150, 210);
   expressionSlider.addClass("mySliders");
 
