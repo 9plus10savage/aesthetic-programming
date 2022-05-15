20c7d314750eff67479142ab7393eae81910aefd
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2022-05-15 21:33:29 +0000

    Upload New File

---
+++ miniX11/class.js
@@ -0,0 +1,16 @@
+class Star {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.size = random(0.25, 1);
+		this.t = random(TAU);
+	}
+
+	show() {
+		let scale = this.size + sin(this.t) * 2;
+		this.t += 0.2
+		fill(255)
+		noStroke();
+		ellipse(this.x, this.y, scale, scale);
+	}
+}
